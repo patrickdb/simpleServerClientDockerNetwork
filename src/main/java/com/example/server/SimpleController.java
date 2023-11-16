6735f36db35e34ab20701c752e7e4079a4776879
Patrick de Beer <p.debeer@fontys.nl>
2023-11-16 10:53:40 +0100

    update changes project

--- src/main/java/com/example/server/SimpleController.java
+++ src/main/java/com/example/server/SimpleController.java
@@ -1,2 +1,17 @@
-package com.example.server;public class SimpleController {
+package com.example.server;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(originPatterns = {"http://localhost", "http://localhost:*"})
+@RequestMapping("/api")
+public class SimpleController {
+
+   @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
 }
